4e18e17cee235c60862ba72f03a45abe5bba4b5e
chorn <chornyyaroslav5@gmail.com>
2023-09-10 20:05:15 +0200

    update

---
+++ del_subscribers.py
@@ -0,0 +1,33 @@
+import asyncio
+import logging
+import sys
+from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ParseMode
+from aiogram.filters import Command
+
+TOKEN = "6616801687:AAFwHV6PONhUJmClAWBKFVglYRPKkJfG6Fw"
+
+bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+
+CHAT_ID = '-1001918816977'
+
+dp = Dispatcher()
+
+
+@dp.channel_post()
+@dp.message(Command('remove'))
+async def echo_handler(message: types.Message) -> None:
+    try:
+        await bot.ban_chat_member(chat_id=CHAT_ID, user_id=message.text.split()[1])
+        await bot.send_message(message.from_user.id, 'Пользователь был забанен')
+    except Exception as e:
+        print(e)
+
+
+async def main() -> None:
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
